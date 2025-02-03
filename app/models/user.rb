f5f2e4eaabcb3bd84153f67f7ad744d545d04acc
PAVLOS IASON BARLAS <92943479+JasonBarlas@users.noreply.github.com>
2025-02-03 18:31:58 +0200

    Update to the User Model. Update to the Application View

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   # Validations
-  validates :username, presence: true, length: { minimum: 4, maximum: 16 }, uniqueness: false
+  validates :username, presence: true, format: { without: /\s/ }, length: { minimum: 4, maximum: 16 }, uniqueness: false
 
   has_many :posts, dependent: :destroy
 end
