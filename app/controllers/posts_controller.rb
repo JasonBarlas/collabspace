fa6a5d7f67c4e8d8e9b0b61c016e5b5694b2f5fa
PAVLOS IASON BARLAS <92943479+JasonBarlas@users.noreply.github.com>
2025-02-04 18:10:59 +0200

    Implemented filters for Posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
-    def show
-        @post = Post.find(params[:id])
-        render json: { title: @post.title, content: @post.content }
-    end      
+  def show
+    @post = Post.find(params[:id])
+    render json: { title: @post.title, content: @post.content }
+  end      
 end
---
+++ app/services/post_filter_service.rb
@@ -0,0 +1,33 @@
+class PostFilterService
+  def initialize(posts, filters)
+    @posts = posts
+    @filters = filters
+  end
+
+  def call
+    filter_by_users
+    filter_by_categories
+    filter_by_title
+    @posts
+  end
+
+  private
+
+  def filter_by_users
+    if @filters[:user_ids].present?
+      @posts = @posts.where(user_id: @filters[:user_ids])
+    end
+  end
+
+  def filter_by_categories
+    if @filters[:category_ids].present?
+      @posts = @posts.where(category_id: @filters[:category_ids])
+    end
+  end
+
+  def filter_by_title
+    if @filters[:title].present?
+      @posts = @posts.where("title ILIKE ?", "%#{@filters[:title]}%")
+    end
+  end
+end
