4030bf32eae85b5d93d91c481f0ef0815c370442
PAVLOS IASON BARLAS <92943479+JasonBarlas@users.noreply.github.com>
2025-02-05 11:36:57 +0200

    Made username unique on User Model

--- db/schema.rb
+++ db/schema.rb
@@ -43,6 +43,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_03_131208) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["username"], name: "index_users_on_username"
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 end
